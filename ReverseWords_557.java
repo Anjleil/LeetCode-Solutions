13d5d46c25325620738b6a0eb126e47028b16e55
Anjleil <timofandr@gmail.com>
2023-01-30 12:47:25 +0300

    Go go go go

---
+++ ReverseWords_557.java
@@ -0,0 +1,24 @@
+package AlgorithmsLeetCode_1;
+
+public class ReverseWords_557 {
+
+    public static void main(String[] args) {
+        ReverseWords_557 answerer = new ReverseWords_557();
+        System.out.println(answerer.reverseWords("Go to IO Games?)"));
+    }
+
+    public String reverseWords(String s) {
+        StringBuilder result = new StringBuilder();
+        StringBuilder reversed = new StringBuilder();
+        String[] strings = s.split(" ");
+        for (int i = 0; i < strings.length; i++){
+            reversed.append(strings[i]).reverse();
+            result.append(reversed);
+            if (i != strings.length - 1){
+                result.append(' ');
+            }
+            reversed = new StringBuilder();
+        }
+        return result.toString();
+    }
+}
